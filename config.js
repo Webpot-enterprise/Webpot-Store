542935778c52f3f6b2ddcd436e762379ee00f3b1
Webpot-enterprise <engagewebpot@gmail.com>
2026-01-13 01:05:42 +0530

    kk

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Webpot Global Configuration
 const WEBPOT_CONFIG = {
     OAUTH_CLIENT_ID: '522296612988-phrs7trh1l6ghauk2khm1181s4a5mvl1.apps.googleusercontent.com',
-    API_URL: 'https://script.google.com/macros/s/AKfycbxK-r-u9FsZanN8h2PhnJBbaimnY6zTrTf9M5QXDT1hdgUv-ofy8RwaSRn_EzcE2-RQ/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbxRdCTFMS36AYDA9znHx9gKrEEJKVHEyxL9ub85QtafCzzQvr6-llHaMwCuegB0Rkxr/exec'
 };
